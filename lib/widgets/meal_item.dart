d4c0ba5016d084955501f3c2abeeba91cf17f31c
Carl Santos <cjassantos2@gmail.com>
2024-02-18 23:59:26 -0800

    Working on Stack Widgets

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+
+    );
+  }
+}
+
