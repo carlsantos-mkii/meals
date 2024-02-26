f1a008027e3aca398074bd3415849f1a4a63928b
Carl Santos <cjassantos2@gmail.com>
2024-02-25 23:38:44 -0800

    Adding meal icon and label

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 class MealItemTrait extends StatelessWidget {
-  const MealItemTrait({super.key});
+  const MealItemTrait({
+    super.key,
+    required this.icon,
+    required this.label
+  });
+
+  final IconData icon;
+  final String label;
 
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Row(children: [
+      Icon(icon, size: 16),
+      const SizedBox(height: 10,),
+      Text(label, style: const TextStyle(color: Color.fromARGB(255, 255, 255, 255)),)
+    ],);
   }
 }
