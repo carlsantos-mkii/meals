993ce09d67e573f5079d933ebf60fdfe7266d248
Carl Santos <cjassantos2@gmail.com>
2024-03-10 22:42:50 -0700

    Adding favorites feature

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
+import 'package:meals/models/meal.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
 
   @override
   State<TabsScreen> createState() {
-    return TabsScreenState();
+    return _TabsScreenState();
   }
 }
 
-class TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+
+  void _toggleMealFavorites(Meal meal) {
+    final isExisting = _favoriteMeals.contains(meal);
+
+    if(isExisting) {
+      _favoriteMeals.remove(meal);
+    } else {
+      _favoriteMeals.add(meal);
+    }
+  }
 
   void _selectPage(int index) {
     setState(() {
