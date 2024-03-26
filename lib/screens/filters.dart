fa3a2a2eaba54a7002bbc5ff625eb884f3c7a871
Carl Santos <cjassantos2@gmail.com>
2024-03-25 23:32:39 -0700

    Adding filters screen

---
+++ lib/screens/filters.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() {
+    return _FiltersScreen();
+  }
+}
+
+class _FiltersScreen extends State<FiltersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
