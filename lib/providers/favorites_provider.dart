efe7da59fe87c1baa24985f6911495d6a318e4c6
Carl Santos <cjassantos2@gmail.com>
2024-04-06 23:50:51 -0700

    Using StateNotifier

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+}
+
+final favoriteMealsProvider = StateNotifierProvider();
