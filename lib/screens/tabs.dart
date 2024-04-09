c57b9e904aee943cef12ea1dfaeac9deacc63c0c
Carl Santos <cjassantos2@gmail.com>
2024-04-08 23:58:40 -0700

    Using the favorite meals provider

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -7,6 +7,7 @@ import 'package:meals/screens/meals.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/widgets/main_drawer.dart';
 import 'package:meals/providers/meals_provider.dart';
+import 'package:meals/providers/favorites_provider.dart';
 
 const KInitialFilters = {
   Filter.glutenFree: false,
